bfa6e9daa57bc15c74bb6319586423b439369b71
xiongyongfeng <2772552493@qq.con>
2024-07-05 09:23:05 +0800

    1.更新代码
    Signed-off-by: xiongyongfeng <2772552493@qq.con>

--- src/gtime/gtime.go
+++ src/gtime/gtime.go
@@ -6,6 +6,7 @@ type baseType interface {
 	int | int8 | int32 | int64 | uint8 | uint32 | uint64 | float32 | float64
 }
 
+//获取时间戳
 func GetTimestamp[T baseType]() T {
 	ltime := time.Now().Unix()
 	return T(ltime)
