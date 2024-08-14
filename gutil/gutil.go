42529e6b264f937b1e0d90aae44ab91baf95258a
xiongyongfeng <2772552493@qq.con>
2024-08-14 10:06:35 +0800

    1.提交
    Signed-off-by: xiongyongfeng <2772552493@qq.con>

--- gutil/gutil.go
+++ gutil/gutil.go
@@ -92,6 +92,7 @@ func CheckIDCard(idCard string) bool {
 
 // CheckMobileNumber 验证手机号码格式是否正确
 func CheckMobileNumber(phoneNumber string) bool {
+
 	// 匹配中国大陆手机号码的正则表达式
 	pattern := `^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$`
 	matched, _ := regexp.MatchString(pattern, phoneNumber)
